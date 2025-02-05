bbe82dbb9722d3dbc35d18ccc588c3b327d64260
Raj Kumar Tharmaraj <rajkumart@scrunchworks.com>
2025-02-05 19:05:02 +0530

    Added new feature

--- src/main/java/com/student/repository/StudentRepository.java
+++ src/main/java/com/student/repository/StudentRepository.java
@@ -10,7 +10,7 @@ import com.student.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-	@Query(value = "SELECT * FROM student ORDER BY marks DESC LIMIT 1 OFFSET 2", nativeQuery = true)
+	@Query(value = "SELECT * FROM student ORDER BY marks DESC LIMIT 1 OFFSET 1", nativeQuery = true)
     Optional<Student> findSecondHighestMarksStudent();
 	
 }
